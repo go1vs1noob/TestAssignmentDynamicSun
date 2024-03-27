b9735227090c13d077b0314c9ef460db9b8f0983
Кривоченко Дмитрий <dima.krivochenko@mail.ru>
2024-03-27 16:59:25 +0300

    Change update values logic

--- AssignmentDynamicSun/Interfaces/IExcelData.cs
+++ AssignmentDynamicSun/Interfaces/IExcelData.cs
@@ -3,8 +3,8 @@
 	public interface IExcelData : IEnumerable<List<string?>>
 	{
 		public IFormFile ExcelFile { get; set; }
-		public string this[int row, int column] { get; }
-		public List<string> this[int row] { get; }
+		public string? this[int row, int column] { get; }
+		public List<string?> this[int row] { get; }
 		public int Count { get; }
 		public Task ReadFromExcelFile(int skipRowsFromStartInSheet);
 	}
